4f31a0b60982c0daa44d18548796c2d669cae208
itsRikizz <pain.sanmay@gmail.com>
2023-06-20 22:01:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const paras = document.querySelectorAll(".paras");
+
+console.log(paras);
+
+const people = ["sanmay", "riki", "Rahul"];
+
+let data = [];
+people.forEach((item) => {
+  //   console.log(item);
+  data.push(item);
+});
+console.log(data);
+paras.forEach((item) => {
+  //   console.log(data);
+  //   data.forEach((items) => {
+  //     item.innerHTML += `<span>${items}</span>`;
+  //   });
+
+  item += data[0];
+});
